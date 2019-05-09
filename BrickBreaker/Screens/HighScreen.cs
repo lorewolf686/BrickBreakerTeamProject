2f1b8fa4170607a270fbf13af7a279a219712571
Andrew Henderson <andrhend062@ed.amdsb.ca>
2019-05-09 09:38:47 -0400

    YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- BrickBreaker/Screens/HighScreen.cs
+++ BrickBreaker/Screens/HighScreen.cs
@@ -39,9 +39,9 @@ namespace BrickBreaker
 		{
 //			Form1.highScores.Sort();
 			highScores.Text = "";
-			for (int i = 0; i < 9; i++)
+			for (int i = 0; i < 10; i++)
 			{
-				highScores.Text += i + ". " + Form1.highScores[i].name + " - " + Form1.highScores[i].score + "\n" + "\n";
+				highScores.Text += i+1 + ". " + Form1.highScores[i].name + " - " + Form1.highScores[i].score + "\n" + "\n";
 			}	
 		}
     }
